b2a95f8622c714fdc081a3705944869420bc32f0
QR1126 <1193523825@qq.com>
2022-06-04 21:08:39 +0800

    [CS61C Classify] Finish Part B

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -27,16 +27,123 @@
 read_matrix:
 
 	# Prologue
+	# store register value on the stack
+	addi sp, sp, -24
+	sw ra, 0(sp) 
+	sw s0, 4(sp)  # s0 is used to store file descriptor return by fopen
+	sw s1, 8(sp)  # s1 is used to store value in a1 and row number
+	sw s2, 12(sp) # s2 is used to store value in a2 and col number
+	sw s3, 16(sp) # s3 is used to store pointer to the matrix in memory
+	sw s4, 20(sp) 
 
+	add s1, a1, x0 # store the pointer
+	add s2, a2, x0 # store the pointer
 
+	# step 1: open the file with read permissions, we will use fopen
+	# Arguments:
+	#   a0 (char *) is the pointer to the filename string
+	#   a1 (int)   is the permission bits. 0 for read-only, 1 for write-only
+	# Returns:
+	#   a0 (int)   A file descriptor. If opening the file failed, this value is -1
+	add a1, x0, x0 # read permission
+	jal fopen
+	li t0, -1
+	beq a0, t0, read_matrix_fopen_exception
+	add s0, a0, x0 # store the file descriptor
 
+	# step 2: Read the number of rows and columns from the file 
+	# (remember: these are the first two integers in the file). 
+	# Store these integers in memory at the provided pointers 
+	# (a1 for rows and a2 for columns). we will use fread
+	# Arguments:
+	#   a0 (int)   is the file descriptor
+	#   a1 (int *) is the pointer to the buffer where the read bytes will be stored
+	#              The buffer should have been previously allocated with malloc
+	#   a2 (int)   is the number of bytes to read from the file
+	# Returns:
+	#   a0 (int)   is the number actually read from the file. If this differs from the argument provided in a2, 
+	#              then we either hit the end of the file or there was an error.
+	# read row number
+	add a0, s0, x0
+	add a1, s1, x0
+	addi a2, x0, 4
+	jal fread
+	li t0, 4
+	bne a0, t0, read_matrix_fread_exception
+	# read col number
+	add a0, s0, x0
+	add a1, s2, x0
+	addi a2, x0, 4
+	jal fread
+	li t0, 4
+	bne a0, t0, read_matrix_fread_exception
 
+	lw s1, 0(s1) # now s1 store row number
+	lw s2, 0(s2) # now s2 store col number
 
+	# step 3: Allocate space on the heap to store the matrix. 
+	# Arguments:
+	#   a0 (int)    The size of the memory that we want to allocate (in bytes)
+	# Returns:
+	#   a0 (void *) A pointer to the allocated memory. If the allocation failed, this value is 0.
+	mul a0, s1, s2
+	slli a0, a0, 2
+	jal malloc
+	beq a0, x0, read_matrix_malloc_exception
+	add s3, a0, x0
 
+	# step 4: Read the matrix from the file to the memory allocated in the previous step
+	mul t0, s1, s2
+	li t1, 0 # i = 0
+	mv s4, s3
+loop_begin:
+	add a0, s0, x0
+	add a1, s4, x0
+	li a2, 4
+	addi sp, sp, -12
+	sw t0, 0(sp)
+	sw t1, 4(sp)
+	sw a2, 8(sp)
+	jal fread
+	lw t0, 0(sp)
+	lw t1, 4(sp)
+	lw a2, 8(sp)
+	addi sp, sp, 12
+	bne a0, a2, read_matrix_fread_exception
+	addi t1, t1, 1
+	beq t1, t0, loop_end
+	addi s4, s4, 4
+	j loop_begin
 
-
-
+loop_end:
 	# Epilogue
+	mv a0, s0
+	jal fclose
+	li t0, -1
+	beq a0, t0, read_matrix_fclose_exception
 
-
+	mv a0, s3
+	lw ra, 0(sp) 
+	lw s0, 4(sp)  # s0 is used to store file descriptor return by fopen
+	lw s1, 8(sp)  # s1 is used to store value in a1 and row number
+	lw s2, 12(sp) # s2 is used to store value in a2 and col number
+	lw s3, 16(sp) # s3 is used to store pointer to the matrix in memory
+	lw s4, 20(sp)
+	addi sp, sp, 24
 	ret
+
+read_matrix_fopen_exception:
+	li a0, 27
+	j exit
+
+read_matrix_fread_exception:
+	li a0, 29
+	j exit
+
+read_matrix_malloc_exception:
+	li a0, 26
+	j exit
+
+read_matrix_fclose_exception:
+	li a0, 28
+	j exit
--- src/write_matrix.s
+++ src/write_matrix.s
@@ -26,15 +26,110 @@ write_matrix:
 
 	# Prologue
 
+	# Open the file with write permissions. The filepath is provided as an argument.
+	# Write the number of rows and columns to the file. (Hint: The fwrite function expects a pointer to data in memory, so you should first store the data to memory, and then pass a pointer to the data to fwrite.)
+	# Write the data to the file.
+	# Close the file.
+	addi sp, sp, -32
+	sw ra, 0(sp) 
+	sw s0, 4(sp)  
+	sw s1, 8(sp)  
+	sw s2, 12(sp) 
+	sw s3, 16(sp) 
+	sw s4, 20(sp) 
+	sw s5, 24(sp)
+	sw s6, 28(sp)
 
+	add s1, a1, x0 # s1 store the pointer to the matrix in memory
+	add s2, a2, x0 # s2 store the number of row
+	add s3, a3, x0 # s3 store the number of col
 
+	li a1, 1
+	jal fopen
+	li t0, -1
+	beq a0, t0, write_matrix_fopen_exception
+	add s0, a0, x0 # s0 store the fd
 
+	# fwrite: Write bytes from a buffer in memory to a file. 
+	# Subsequent writes append to the end of the existing file.
+	# Arguments:
+	#   a0 (int) 	fd
+	#   a1 (void *) A pointer to a buffer containing what we want to write to the file
+	#   a2 (int)    The number of elements to write to the file
+	#   a3 (int)    The size of each element. In total, a2 × a3 bytes are written
+	# Returns:
+	#   a0 (int)    The number of items actually written to the file
 
+	addi a0, a0, 4
+	jal malloc
+	beq a0, x0, write_matrix_malloc_exception
+	mv s4, a0 # s4 store the pointer to a buffer that contain row number
+	sw s2, 0(s4)
 
+	addi a0, a0, 4
+	jal malloc
+	beq a0, x0, write_matrix_malloc_exception
+	mv s5, a0 # s5 store the pointer to a buffer that contain col number
+	sw s3, 0(s5)
 
+	# write row number to the file
+	add a0, s0, x0
+	add a1, s4, x0
+	li a2, 1
+	li a3, 4
+	jal fwrite
+	li t0, 1
+	bne a0, t0, write_matrix_fwrite_exception
 
+	# write col number to the file
+	add a0, s0, x0
+	add a1, s5, x0
+	li a2, 1
+	li a3, 4
+	jal fwrite
+	li t0, 1
+	bne a0, t0, write_matrix_fwrite_exception
+
+	# write matrix data to the file
+	mul t0, s2, s3
+	add a0, s0, x0
+	add a1, s1, x0
+	add a2, t0, x0
+	addi a3, x0, 4
+	jal fwrite
+	mul t0, s2, s3
+	bne a0, t0, write_matrix_fwrite_exception
+
+	# close the file
+	add a0, s0, x0
+	jal fclose
+	li t0, -1
+	beq a0, t0, write_matrix_fclose_exception
 
 	# Epilogue
-
-
+	lw ra, 0(sp) 
+	lw s0, 4(sp)  
+	lw s1, 8(sp)  
+	lw s2, 12(sp) 
+	lw s3, 16(sp) 
+	lw s4, 20(sp) 
+	lw s5, 24(sp)
+	lw s6, 28(sp)
+	addi sp, sp, 32
 	ret
+
+write_matrix_fopen_exception:
+	li a0, 27
+	j exit
+
+write_matrix_malloc_exception:
+	li a0, 26
+	j exit
+
+write_matrix_fwrite_exception:
+	li a0, 30
+	j exit
+
+write_matrix_fclose_exception:
+	li a0, 28
+	j exit
